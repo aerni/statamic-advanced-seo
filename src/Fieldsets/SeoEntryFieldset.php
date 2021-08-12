df656bb95e0eec6561ea88f17b25c1c93d144947
Michael Aerni <hello@michaelaerni.ch>
2021-08-12 10:40:24 +0200

    Disassemble seo entry fieldset
    Users can now also provide their own `social_images_generator_fields` fieldset, which will be merged right below the Social Images Generator.

--- src/Fieldsets/SeoEntryFieldset.php
+++ src/Fieldsets/SeoEntryFieldset.php
@@ -12,12 +12,64 @@ class SeoEntryFieldset extends BaseFieldset
     protected function sections(): array
     {
         return [
-            $this->entrySeo(),
+            $this->seoTags(),
+            $this->socialImagesGenerator(),
+            $this->socialImagesGeneratorFields(),
+            $this->openGraph(),
+            $this->twitter(),
+            $this->canonicalUrl(),
+            $this->indexing(),
+            $this->sitemap(),
+            $this->jsonLd(),
         ];
     }
 
-    protected function entrySeo(): Collection
+    protected function seoTags(): Collection
     {
-        return Fieldset::find('entry_seo');
+        return Fieldset::find('entry/seo_tags');
+    }
+
+    protected function socialImagesGenerator(): ?Collection
+    {
+        return config('advanced-seo.social_images.generator.enabled', true)
+            ? Fieldset::find('entry/social_images_generator')
+            : null;
+    }
+
+    protected function socialImagesGeneratorFields(): ?Collection
+    {
+        return config('advanced-seo.social_images.generator.enabled', true)
+            ? Fieldset::find('social_images_generator_fields')
+            : null;
+    }
+
+    protected function openGraph(): Collection
+    {
+        return Fieldset::find('entry/open_graph');
+    }
+
+    protected function twitter(): Collection
+    {
+        return Fieldset::find('entry/twitter');
+    }
+
+    protected function canonicalUrl(): Collection
+    {
+        return Fieldset::find('entry/canonical_url');
+    }
+
+    protected function indexing(): Collection
+    {
+        return Fieldset::find('entry/indexing');
+    }
+
+    protected function sitemap(): Collection
+    {
+        return Fieldset::find('entry/sitemap');
+    }
+
+    protected function jsonLd(): Collection
+    {
+        return Fieldset::find('entry/json_ld');
     }
 }
