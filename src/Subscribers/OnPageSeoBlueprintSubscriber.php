2ee1b5645126c9448d2ea6897092bbfc53741795
Michael Aerni <hello@michaelaerni.ch>
2021-10-22 15:00:35 +0200

    Also merge defaults for terms

--- src/Subscribers/OnPageSeoBlueprintSubscriber.php
+++ src/Subscribers/OnPageSeoBlueprintSubscriber.php
@@ -4,8 +4,7 @@ namespace Aerni\AdvancedSeo\Subscribers;
 
 use Aerni\AdvancedSeo\Blueprints\OnPageSeoBlueprint;
 use Aerni\AdvancedSeo\Jobs\GenerateSocialImageJob;
-use Aerni\AdvancedSeo\Repositories\CollectionDefaultsRepository;
-use Aerni\AdvancedSeo\Repositories\TaxonomyDefaultsRepository;
+use Aerni\AdvancedSeo\Repositories\SeoDefaultsRepository;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Support\Str;
 use Statamic\Events;
@@ -15,8 +14,10 @@ class OnPageSeoBlueprintSubscriber
 {
     protected array $events = [
         Events\EntryBlueprintFound::class => 'addFieldsToBlueprint',
-        Events\EntrySaving::class => 'removeDefaultDataFromEntry',
         Events\TermBlueprintFound::class => 'addFieldsToBlueprint',
+        Events\EntrySaving::class => 'removeDefaultDataFromEntry',
+        // TODO: This event does not currently exist but will be added with an open PR.
+        // Events\TermSaving::class => 'removeDefaultDataFromEntry',
         Events\CollectionSaved::class => 'createOrDeleteLocalizations',
         Events\TaxonomySaved::class => 'createOrDeleteLocalizations',
         Events\CollectionDeleted::class => 'deleteDefaults',
@@ -39,46 +40,83 @@ class OnPageSeoBlueprintSubscriber
 
         $event->blueprint->ensureFieldsInSection(OnPageSeoBlueprint::make()->items(), 'SEO');
 
-        $this->addDefaultDataToEntry($event);
+        property_exists($event, 'entry')
+            ? $this->addDefaultDataToEntry($event)
+            : $this->addDefaultDataToTerm($event);
     }
 
     /**
-     * TODO: This has to also work with Terms.
-     * Makes sure that we only save data that is different to the default data.
-     * This ensures that the blueprint always loads the latest default data if no other value has been set on the entry.
-     */
-    public function removeDefaultDataFromEntry(Event $event): void
-    {
-        if ($event->entry) {
-            $collection = $event->entry->collection();
-            $defaults = (new CollectionDefaultsRepository($collection->handle(), $collection->sites()))->set()->in($event->entry->locale())->data();
-
-            $diffed = $event->entry->data()->filter(function ($value, $key) use ($defaults) {
-                return $value !== $defaults->get($key);
-            });
-
-            $event->entry->data($diffed);
-        }
-    }
-
-    /**
-     * TODO: This has to also work with Terms.
      * Adds the content defaults to the entry.
      * It only adds values if they have not already been set on the entry.
      */
     protected function addDefaultDataToEntry(Event $event): void
     {
-        if ($event->entry) {
-            $collection = $event->entry->collection();
-            $defaults = (new CollectionDefaultsRepository($collection->handle(), $collection->sites()))->set()->in($event->entry->locale())->data();
-
-            // We only want to set the defaults that were not changed on the entry.
-            $entryData = $event->entry->data();
-            $defaultsToSet = $defaults->diffKeys($entryData);
-            $newValues = $entryData->merge($defaultsToSet)->toArray();
-
-            $event->entry->data($newValues);
+        if (! $event->entry) {
+            return;
         }
+
+        $collection = $event->entry->collection();
+        $defaults = (new SeoDefaultsRepository('collections', $collection->handle(), $collection->sites()))->set()->in($event->entry->locale())->data();
+
+        // We only want to set the defaults that were not changed on the entry.
+        $entryData = $event->entry->data();
+        $defaultsToSet = $defaults->diffKeys($entryData);
+        $mergedData = $entryData->merge($defaultsToSet)->toArray();
+
+        $event->entry->data($mergedData);
+    }
+
+    /**
+     * Adds the content defaults to the term.
+     * It only adds values if they have not already been set on the term.
+     */
+    protected function addDefaultDataToTerm(Event $event): void
+    {
+        if (! $event->term) {
+            return;
+        }
+
+        $taxonomy = $event->term->taxonomy();
+        $defaults = (new SeoDefaultsRepository('taxonomies', $taxonomy->handle(), $taxonomy->sites()))->set()->in($event->term->locale())->data();
+
+        // We only want to set the defaults that were not changed on the entry.
+        $termData = $event->term->data();
+        $defaultsToSet = $defaults->diffKeys($termData);
+        $mergedData = $termData->merge($defaultsToSet)->toArray();
+
+        $event->term->data($mergedData);
+    }
+
+    /**
+     * Makes sure that we only save data that is different to the default data.
+     * This ensures that the blueprint always loads the latest default data if no other value has been set on the entry.
+     */
+    public function removeDefaultDataFromEntry(Event $event): void
+    {
+        $collection = $event->entry->collection();
+        $defaults = (new SeoDefaultsRepository('collections', $collection->handle(), $collection->sites()))->set()->in($event->entry->locale())->data();
+
+        $dataWithoutDefaults = $event->entry->data()->filter(function ($value, $key) use ($defaults) {
+            return $value !== $defaults->get($key);
+        });
+
+        $event->entry->data($dataWithoutDefaults);
+    }
+
+    /**
+     * Makes sure that we only save data that is different to the default term.
+     * This ensures that the blueprint always loads the latest default data if no other value has been set on the term.
+     */
+    public function removeDefaultDataFromTerm(Event $event): void
+    {
+        $taxonomy = $event->term->taxonomy();
+        $defaults = (new SeoDefaultsRepository('taxonomies', $taxonomy->handle(), $taxonomy->sites()))->set()->in($event->term->locale())->data();
+
+        $dataWithoutDefaults = $event->term->data()->filter(function ($value, $key) use ($defaults) {
+            return $value !== $defaults->get($key);
+        });
+
+        $event->term->data($dataWithoutDefaults);
     }
 
     /**
