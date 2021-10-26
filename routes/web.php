513e7f4bc4aa7cb6c9e23a85518504c26673c148
Michael Aerni <hello@michaelaerni.ch>
2021-10-26 13:55:33 +0200

    Disable routes if generator is disabled

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use Statamic\Facades\Site;
 
-Site::all()->each(function ($site) {
-    Route::statamic("{$site->url()}/seo/social-images/og/{id}", 'social_images/og', [
-        'layout' => 'social_images/layout',
-    ]);
+if (config('advanced-seo.social_images.generator.enabled', false)) {
+    Site::all()->each(function ($site) {
+        Route::statamic("{$site->url()}/seo/social-images/og/{id}", 'social_images/og', [
+            'layout' => 'social_images/layout',
+        ]);
 
-    Route::statamic("{$site->url()}/seo/social-images/twitter/{id}", 'social_images/twitter', [
-        'layout' => 'social_images/layout',
-    ]);
-});
+        Route::statamic("{$site->url()}/seo/social-images/twitter/{id}", 'social_images/twitter', [
+            'layout' => 'social_images/layout',
+        ]);
+    });
+}
