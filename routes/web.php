94b3ac785edbf2cc8ba8b43ab1dc91b37367a9db
Michael Aerni <hello@michaelaerni.ch>
2021-12-02 09:35:46 +0100

    Extract social images routes to controller

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,13 @@
 use Statamic\Facades\Site;
 use Illuminate\Support\Facades\Route;
 use Aerni\AdvancedSeo\Http\Controllers\Web\SitemapController;
+use Aerni\AdvancedSeo\Http\Controllers\Web\SocialImagesController;
 
-// Social Images Generator
-if (config('advanced-seo.social_images.generator.enabled', false)) {
-    Site::all()->each(function ($site) {
-        Route::statamic("{$site->url()}/seo/social-images/og/{id}", 'social_images/og', [
-            'layout' => 'social_images/layout',
-        ]);
-
-        Route::statamic("{$site->url()}/seo/social-images/twitter/{id}", 'social_images/twitter', [
-            'layout' => 'social_images/layout',
-        ]);
-    });
-}
-
-// Sitemap
 Site::all()->map(function ($site) {
+    // Social Images Generator
+    Route::get($site->url() . '/social-images/{type}/{id}', [SocialImagesController::class, 'show'])->name('advanced-seo.social_images.show');
+
+    // Sitemap
     Route::get($site->url() . '/sitemap.xml', [SitemapController::class, 'index'])->name('advanced-seo.sitemap.index');
     Route::get($site->url() . '/sitemap_{type}_{handle}.xml', [SitemapController::class, 'show'])->name('advanced-seo.sitemap.show');
     Route::get($site->url() . '/sitemap.xsl', [SitemapController::class, 'xsl'])->name('advanced-seo.sitemap.xsl');
---
+++ src/Http/Controllers/Web/SocialImagesController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Aerni\AdvancedSeo\Http\Controllers\Web;
+
+use Aerni\AdvancedSeo\Facades\SocialImage;
+use Statamic\View\View;
+use Statamic\Facades\Data;
+use Illuminate\Routing\Controller;
+use Statamic\Contracts\Entries\Entry;
+use Statamic\Exceptions\NotFoundHttpException;
+use Statamic\Taxonomies\LocalizedTerm;
+
+class SocialImagesController extends Controller
+{
+    public function show(string $type, string $id): View
+    {
+        // Throw if the social images generator is disabled.
+        throw_unless(config('advanced-seo.social_images.generator.enabled', false), new NotFoundHttpException);
+
+        // Throw if the social image type is not supported.
+        throw_unless($specs = SocialImage::types()->get($type), new NotFoundHttpException);
+
+        // Throw if no data was found.
+        throw_unless($data = Data::find($id), new NotFoundHttpException);
+
+        // Throw if the data is not an entry or term.
+        throw_unless($data instanceof Entry || $data instanceof LocalizedTerm, new NotFoundHttpException());
+
+        return (new View)
+            ->template("social_images/$type")
+            ->layout('social_images/layout')
+            ->with($data->merge($specs)->toAugmentedArray());
+    }
+}
