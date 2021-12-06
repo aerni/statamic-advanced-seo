6e2564c0e85a456ea2668b866e37df6ee19da23a
Michael Aerni <hello@michaelaerni.ch>
2021-12-06 13:48:41 +0100

    Get the correct fields

--- src/Traits/GetsFieldsWithDefault.php
+++ src/Traits/GetsFieldsWithDefault.php
@@ -2,15 +2,34 @@
 
 namespace Aerni\AdvancedSeo\Traits;
 
-use Statamic\Fields\Field;
+use Illuminate\Support\Arr;
+use Statamic\Fields\Blueprint;
+use Illuminate\Support\Collection;
 
 trait GetsFieldsWithDefault
 {
-    public function getFieldsWithDefault(array $fields)
+    use GetsContentDefaults;
+
+    /**
+     * Use the $getFresh argument if the blueprint fields don't return the localized values.
+     */
+    public function getFieldsWithDefault(Blueprint $blueprint, $getFresh = false): Collection
     {
-        return collect($fields)->mapWithKeys(function ($config, $handle) {
-            $field = new Field($handle, $config);
-            return [$field->handle() => $field->preProcess()->value()];
-        })->filter();
+        // Get the fields that have a default value set.
+        $fieldsWithDefaults = $blueprint->fields()->resolveFields()->mapWithKeys(function ($field) {
+            return [$field->handle() => Arr::get($field->config(), 'default')];
+        })->filter(function ($value) {
+            return $value !== null;
+        });
+
+        if (! $getFresh) {
+            return $fieldsWithDefaults;
+        }
+
+        // Get the content defaults for the entry linked to the blueprint.
+        $contentDefaults = collect($this->getContentDefaults($blueprint->getParent()))->map->raw();
+
+        // Return the defaults for the current entry.
+        return $contentDefaults->intersectByKeys($fieldsWithDefaults);
     }
 }
