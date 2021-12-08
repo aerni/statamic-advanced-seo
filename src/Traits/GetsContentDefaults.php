60140056d5ba33312266d4709668f841741abb85
aerni <aerni@users.noreply.github.com>
2021-12-08 14:20:37 +0000

    Fix styling

--- src/Traits/GetsContentDefaults.php
+++ src/Traits/GetsContentDefaults.php
@@ -2,16 +2,16 @@
 
 namespace Aerni\AdvancedSeo\Traits;
 
-use Statamic\Fields\Value;
-use Illuminate\Support\Arr;
-use Statamic\Facades\Blink;
-use Statamic\Taxonomies\Taxonomy;
 use Aerni\AdvancedSeo\Facades\Seo;
-use Statamic\Contracts\Entries\Entry;
-use Statamic\Taxonomies\LocalizedTerm;
-use Statamic\Contracts\Taxonomies\Term;
-use Statamic\Contracts\Entries\Collection;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Collection as LaravelCollection;
+use Statamic\Contracts\Entries\Collection;
+use Statamic\Contracts\Entries\Entry;
+use Statamic\Contracts\Taxonomies\Term;
+use Statamic\Facades\Blink;
+use Statamic\Fields\Value;
+use Statamic\Taxonomies\LocalizedTerm;
+use Statamic\Taxonomies\Taxonomy;
 
 trait GetsContentDefaults
 {
