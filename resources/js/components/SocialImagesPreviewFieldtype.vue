99bc01443a876c68d87c6f949b60a2409a7b14e6
Michael Aerni <hello@michaelaerni.ch>
2021-12-10 20:54:10 +0100

    Remove typo

--- resources/js/components/SocialImagesPreviewFieldtype.vue
+++ resources/js/components/SocialImagesPreviewFieldtype.vue
@@ -2,7 +2,7 @@
     <div>
         <img v-if="this.meta.image" :src="this.meta.image">
 
-        <div v-else class="p-3 text-center border rounded sha" style="border-color: #c4ccd4; background-color: #fafcff">
+        <div v-else class="p-3 text-center border rounded" style="border-color: #c4ccd4; background-color: #fafcff">
             <small class="mb-0 help-block">Save the entry to generate your first {{ this.meta.title }} image.</small>
         </div>
     </div>
