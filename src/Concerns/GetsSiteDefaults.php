a229b845e196a9954fd4284ac77b42dce8176f70
Michael Aerni <hello@michaelaerni.ch>
2022-02-07 12:12:41 +0100

    Optimize by only getting the locale once

--- src/Concerns/GetsSiteDefaults.php
+++ src/Concerns/GetsSiteDefaults.php
@@ -6,27 +6,31 @@ use Statamic\Fields\Value;
 use Statamic\Facades\Blink;
 use Aerni\AdvancedSeo\Facades\Seo;
 use Illuminate\Support\Collection;
-use Statamic\Contracts\Entries\Entry;
-use Statamic\Contracts\Taxonomies\Term;
 
 trait GetsSiteDefaults
 {
     use GetsLocale;
 
-    public function getSiteDefaults(Entry|Term|array $data): Collection
+    /**
+     * Get the augmented site defaults and filter out any values that shouldn't be there,
+     * like features that were disabled in the config.
+     */
+    public function getSiteDefaults(mixed $data = null): Collection
     {
-        return Blink::once($this->getSiteCacheKey($data), function () use ($data) {
-            return Seo::allOfType('site')->flatMap(function ($defaults) use ($data) {
-                return $defaults->in($this->getLocale($data))?->toAugmentedArray();
+        $locale = $this->getLocale($data);
+
+        return Blink::once($this->getSiteCacheKey($locale), function () use ($locale) {
+            return Seo::allOfType('site')->flatMap(function ($defaults) use ($locale) {
+                return $defaults->in($locale)?->toAugmentedArray();
             })->filter(function ($item) {
                 // Only return values that have a corresponding field in the blueprint.
-                return $item instanceof Value;
+                return $item instanceof Value && $item->raw();
             });
         });
     }
 
-    protected function getSiteCacheKey(Entry|Term|array $data): string
+    protected function getSiteCacheKey(string $locale): string
     {
-        return "advanced-seo::site::all::{$this->getLocale($data)}";
+        return "advanced-seo::site::all::$locale";
     }
 }
