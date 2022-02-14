cd69e5181bdf97e367b61c36e5582d4a93830511
Michael Aerni <hello@michaelaerni.ch>
2022-02-14 10:15:09 +0100

    Add support for Statamic's Git integration

--- src/Events/SeoDefaultSetSaved.php
+++ src/Events/SeoDefaultSetSaved.php
@@ -2,13 +2,19 @@
 
 namespace Aerni\AdvancedSeo\Events;
 
-use Aerni\AdvancedSeo\Data\SeoDefaultSet;
 use Statamic\Events\Event;
+use Aerni\AdvancedSeo\Data\SeoDefaultSet;
+use Statamic\Contracts\Git\ProvidesCommitMessage;
 
-class SeoDefaultSetSaved extends Event
+class SeoDefaultSetSaved extends Event implements ProvidesCommitMessage
 {
     public function __construct(public SeoDefaultSet $defaults)
     {
         //
+    }
+
+    public function commitMessage()
+    {
+        return __('SEO defaults saved', [], config('statamic.git.locale'));
     }
 }
