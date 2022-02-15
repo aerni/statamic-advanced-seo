eeb36f00bec0d8f0ba9fca7b4a68efce638cacc4
Michael Aerni <hello@michaelaerni.ch>
2022-02-15 09:35:46 +0100

    Group routes with prefix and name

--- routes/cp.php
+++ routes/cp.php
@@ -6,16 +6,16 @@ use Aerni\AdvancedSeo\Http\Controllers\Cp\TaxonomyDefaultsController;
 use Aerni\AdvancedSeo\Http\Controllers\Cp\CollectionDefaultsController;
 use Aerni\AdvancedSeo\Http\Controllers\Cp\OverviewController;
 
-// TODO: Use route group.
+Route::prefix('advanced-seo')->name('advanced-seo.')->group(function () {
+    Route::get('/', [OverviewController::class, 'index'])->name('index');
+    Route::get('/{group}', [OverviewController::class, 'show'])->name('show');
 
-Route::get('advanced-seo', [OverviewController::class, 'index'])->name('advanced-seo.index');
-Route::get('advanced-seo/{group}', [OverviewController::class, 'show'])->name('advanced-seo.show');
+    Route::get('/site/{default}', [SiteDefaultsController::class, 'edit'])->name('site.edit');
+    Route::patch('/site/{default}', [SiteDefaultsController::class, 'update'])->name('site.update');
 
-Route::get('advanced-seo/site/{default}', [SiteDefaultsController::class, 'edit'])->name('advanced-seo.site.edit');
-Route::patch('advanced-seo/site/{default}', [SiteDefaultsController::class, 'update'])->name('advanced-seo.site.update');
+    Route::get('/content/collections/{collection}', [CollectionDefaultsController::class, 'edit'])->name('content.collections.edit');
+    Route::patch('/content/collections/{collection}', [CollectionDefaultsController::class, 'update'])->name('content.collections.update');
 
-Route::get('advanced-seo/content/collections/{collection}', [CollectionDefaultsController::class, 'edit'])->name('advanced-seo.content.collections.edit');
-Route::patch('advanced-seo/content/collections/{collection}', [CollectionDefaultsController::class, 'update'])->name('advanced-seo.content.collections.update');
-
-Route::get('advanced-seo/content/taxonomies/{taxonomy}', [TaxonomyDefaultsController::class, 'edit'])->name('advanced-seo.content.taxonomies.edit');
-Route::patch('advanced-seo/content/taxonomies/{taxonomy}', [TaxonomyDefaultsController::class, 'update'])->name('advanced-seo.content.taxonomies.update');
+    Route::get('/content/taxonomies/{taxonomy}', [TaxonomyDefaultsController::class, 'edit'])->name('content.taxonomies.edit');
+    Route::patch('/content/taxonomies/{taxonomy}', [TaxonomyDefaultsController::class, 'update'])->name('content.taxonomies.update');
+});
--- routes/web.php
+++ routes/web.php
@@ -5,12 +5,14 @@ use Illuminate\Support\Facades\Route;
 use Aerni\AdvancedSeo\Http\Controllers\Web\SitemapController;
 use Aerni\AdvancedSeo\Http\Controllers\Web\SocialImagesController;
 
-Site::all()->map(function ($site) {
-    // Social Images Generator
-    Route::get($site->url() . '/social-images/{type}/{id}', [SocialImagesController::class, 'show'])->name('advanced-seo.social_images.show');
+Route::name('advanced-seo.')->group(function () {
+    Site::all()->map(function ($site) {
+        // Social Images Generator
+        Route::get($site->url() . '/social-images/{type}/{id}', [SocialImagesController::class, 'show'])->name('social_images.show');
 
-    // Sitemap
-    Route::get($site->url() . '/sitemap.xml', [SitemapController::class, 'index'])->name('advanced-seo.sitemap.index');
-    Route::get($site->url() . '/sitemap_{type}_{handle}.xml', [SitemapController::class, 'show'])->name('advanced-seo.sitemap.show');
-    Route::get($site->url() . '/sitemap.xsl', [SitemapController::class, 'xsl'])->name('advanced-seo.sitemap.xsl');
+        // Sitemap
+        Route::get($site->url() . '/sitemap.xml', [SitemapController::class, 'index'])->name('sitemap.index');
+        Route::get($site->url() . '/sitemap_{type}_{handle}.xml', [SitemapController::class, 'show'])->name('sitemap.show');
+        Route::get($site->url() . '/sitemap.xsl', [SitemapController::class, 'xsl'])->name('sitemap.xsl');
+    });
 });
