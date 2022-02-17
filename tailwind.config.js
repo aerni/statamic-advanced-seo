931ef0a85ee4e96ee372acea45aa82608548e005
Michael Aerni <hello@michaelaerni.ch>
2022-02-17 14:55:47 +0100

    Disable preflight to prevent style collisions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 module.exports = {
     prefix: 'seo-',
+
     content: [
         './resources/views/**/*.html',
         './resources/views/**/*.php',
         './resources/js/**/*.js',
         './resources/js/**/*.vue',
     ],
+
+    corePlugins: {
+        preflight: false,
+    },
 }
