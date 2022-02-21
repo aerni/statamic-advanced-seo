921869ef0a5b5607eec611946c0f5db353563b08
Michael Aerni <hello@michaelaerni.ch>
2022-02-21 14:31:29 +0100

    Get site defaults with default values

--- src/Concerns/GetsSiteDefaults.php
+++ src/Concerns/GetsSiteDefaults.php
@@ -3,6 +3,7 @@
 namespace Aerni\AdvancedSeo\Concerns;
 
 use Aerni\AdvancedSeo\Facades\Seo;
+use Aerni\AdvancedSeo\Models\Defaults;
 use Illuminate\Support\Collection;
 use Statamic\Contracts\Entries\Entry;
 use Statamic\Contracts\Taxonomies\Term;
@@ -22,11 +23,12 @@ trait GetsSiteDefaults
         $locale = $this->getLocale($data);
 
         return Blink::once($this->getSiteCacheKey($locale), function () use ($locale) {
-            return Seo::allOfType('site')->flatMap(function ($defaults) use ($locale) {
-                return $defaults->in($locale)?->toAugmentedArray();
-            })->filter(function ($item) {
-                // Only return values that have a corresponding field in the blueprint.
-                return $item instanceof Value && $item->raw() !== null;
+            return Defaults::enabledInGroup('site')->flatMap(function ($model) use ($locale) {
+                return Seo::findOrMake('site', $model['handle'])
+                    ->ensureLocalizations(collect($locale))
+                    ->in($locale)
+                    ->toAugmentedCollection()
+                    ->filter(fn ($item) => $item instanceof Value && $item->raw() !== null);
             });
         });
     }
