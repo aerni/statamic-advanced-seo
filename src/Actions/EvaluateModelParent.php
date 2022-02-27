4431a7ae7a143514aa562953a416311d1d9af827
aerni <aerni@users.noreply.github.com>
2022-02-27 04:26:15 +0000

    Fix styling

--- src/Actions/EvaluateModelParent.php
+++ src/Actions/EvaluateModelParent.php
@@ -3,9 +3,7 @@
 namespace Aerni\AdvancedSeo\Actions;
 
 use Illuminate\Support\Str;
-use Statamic\Contracts\Entries\Collection;
 use Statamic\Contracts\Entries\Entry;
-use Statamic\Contracts\Taxonomies\Taxonomy;
 use Statamic\Contracts\Taxonomies\Term;
 use Statamic\Events\EntryBlueprintFound;
 use Statamic\Events\TermBlueprintFound;
