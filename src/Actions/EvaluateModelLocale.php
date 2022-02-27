36408af7fbbc94891877f65dc8a2ef81631ba9d7
aerni <aerni@users.noreply.github.com>
2022-02-27 01:59:31 +0000

    Fix styling

--- src/Actions/EvaluateModelLocale.php
+++ src/Actions/EvaluateModelLocale.php
@@ -2,14 +2,10 @@
 
 namespace Aerni\AdvancedSeo\Actions;
 
-use Statamic\Contracts\Entries\Collection;
 use Statamic\Contracts\Entries\Entry;
-use Statamic\Contracts\Taxonomies\Taxonomy;
 use Statamic\Contracts\Taxonomies\Term;
 use Statamic\Events\EntryBlueprintFound;
 use Statamic\Events\TermBlueprintFound;
-use Statamic\Facades\Site;
-use Statamic\Statamic;
 use Statamic\Tags\Context;
 
 class EvaluateModelLocale
--- src/Actions/EvaluateModelParent.php
+++ src/Actions/EvaluateModelParent.php
@@ -2,18 +2,18 @@
 
 namespace Aerni\AdvancedSeo\Actions;
 
-use Statamic\Tags\Context;
 use Illuminate\Support\Str;
-use Statamic\Contracts\Entries\Entry;
-use Statamic\Taxonomies\LocalizedTerm;
-use Statamic\Contracts\Taxonomies\Term;
-use Statamic\Events\TermBlueprintFound;
-use Statamic\Events\EntryBlueprintFound;
 use Statamic\Contracts\Entries\Collection;
+use Statamic\Contracts\Entries\Entry;
 use Statamic\Contracts\Taxonomies\Taxonomy;
-use Statamic\Facades\Taxonomy as TaxonomyFacade;
+use Statamic\Contracts\Taxonomies\Term;
+use Statamic\Events\EntryBlueprintFound;
+use Statamic\Events\TermBlueprintFound;
 use Statamic\Facades\Collection as CollectionFacade;
+use Statamic\Facades\Taxonomy as TaxonomyFacade;
 use Statamic\Stache\Query\TermQueryBuilder;
+use Statamic\Tags\Context;
+use Statamic\Taxonomies\LocalizedTerm;
 
 class EvaluateModelParent
 {
