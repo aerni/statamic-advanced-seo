3b696188b2fdd23a1df1f39b97d50f346d4a7d62
Michael Aerni <hello@michaelaerni.ch>
2022-03-07 14:29:34 -0800

    Update field descriptions

--- resources/lang/en/fields.php
+++ resources/lang/en/fields.php
@@ -10,7 +10,7 @@ return [
     ],
 
     'seo_title' => [
-        'instructions' => 'Set the meta title of this :type. This defaults to the :type\'s title.',
+        'instructions' => 'Set the meta title of this :type.',
         'default_instructions' => 'Set the default meta title of your :type.',
     ],
 
@@ -43,18 +43,18 @@ return [
     ],
 
     'seo_og_title' => [
-        'instructions' => 'Set the Open Graph title of this :type. This defaults to the :type\'s meta title.',
+        'instructions' => 'Set the Open Graph title of this :type.',
         'default_instructions' => 'Set the default Open Graph title of your :type.',
     ],
 
     'seo_og_description' => [
-        'instructions' => 'Set the Open Graph description of this :type. This defaults to the :type\'s meta description.',
+        'instructions' => 'Set the Open Graph description of this :type.',
         'default_instructions' => 'Set the default Open Graph description of your :type.',
     ],
 
     'seo_og_image' => [
-        'instructions' => 'Add an Open Graph image for this :type. The image will be cropped to ' . SocialImage::sizeString('og') . '.',
-        'default_instructions' => 'Add a default Open Graph image for your :type. The image will be cropped to ' . SocialImage::sizeString('og') . '.',
+        'instructions' => 'Add an Open Graph image for this :type. It will be cropped to ' . SocialImage::sizeString('og') . '.',
+        'default_instructions' => 'Add a default Open Graph image for your :type. It will be cropped to ' . SocialImage::sizeString('og') . '.',
     ],
 
     'seo_section_twitter' => [
@@ -68,38 +68,38 @@ return [
     ],
 
     'seo_twitter_title' => [
-        'instructions' => 'Set the Twitter title of this :type. This defaults to the :type\'s meta title.',
+        'instructions' => 'Set the Twitter title of this :type.',
         'default_instructions' => 'Set the default Twitter title of your :type.',
     ],
 
     'seo_twitter_description' => [
-        'instructions' => 'Set the Twitter description of this :type. This defaults to the :type\'s meta description.',
+        'instructions' => 'Set the Twitter description of this :type.',
         'default_instructions' => 'Set the default Twitter description of your :type.',
     ],
 
     'seo_twitter_summary_image' => [
-        'instructions' => 'Add a Twitter image for this :type. The image will be cropped to ' . SocialImage::sizeString('twitter.summary') . '.',
-        'default_instructions' => 'Add a default Twitter image for your :type. The image will be cropped to ' . SocialImage::sizeString('twitter.summary') . '.',
+        'instructions' => 'Add a Twitter image for this :type. It will be cropped to ' . SocialImage::sizeString('twitter.summary') . '.',
+        'default_instructions' => 'Add a default Twitter image for your :type. It will be cropped to ' . SocialImage::sizeString('twitter.summary') . '.',
     ],
 
     'seo_twitter_summary_large_image' => [
-        'instructions' => 'Add a Twitter image for this :type. The image will be cropped to ' . SocialImage::sizeString('twitter.summary_large_image') . '.',
-        'default_instructions' => 'Add a default Twitter image for your :type. The image will be cropped to ' . SocialImage::sizeString('twitter.summary_large_image') . '.',
+        'instructions' => 'Add a Twitter image for this :type. It will be cropped to ' . SocialImage::sizeString('twitter.summary_large_image') . '.',
+        'default_instructions' => 'Add a default Twitter image for your :type. It will be cropped to ' . SocialImage::sizeString('twitter.summary_large_image') . '.',
     ],
 
     'seo_section_canonical_url' => [
-        'instructions' => 'Configure the canonical URL settings for this :type.',
-        'default_instructions' => 'Configure the default canonical URL settings for your :type.',
+        'instructions' => 'Configure the canonical URL settings of this :type.',
+        'default_instructions' => 'Configure the default canonical URL settings of your :type.',
     ],
 
     'seo_canonical_type' => [
-        'instructions' => 'Where should the canonical URL for this :type point to.',
-        'default_instructions' => 'Where should the default canonical URL for your :type point to.',
+        'instructions' => 'Where should the canonical URL of this :type point to.',
+        'default_instructions' => 'Where should the default canonical URL of your :type point to.',
     ],
 
     'seo_canonical_entry' => [
-        'instructions' => 'Link to the entry with the original content.',
-        'default_instructions' => 'Link to the entry with the original content.',
+        'instructions' => 'Choose the entry with the original content.',
+        'default_instructions' => 'Choose the entry with the original content.',
     ],
 
     'seo_canonical_custom' => [
@@ -108,8 +108,8 @@ return [
     ],
 
     'seo_section_indexing' => [
-        'instructions' => 'Configure the indexing settings for this :type.',
-        'default_instructions' => 'Configure the default indexing settings for your :type.',
+        'instructions' => 'Configure the indexing settings of this :type.',
+        'default_instructions' => 'Configure the default indexing settings of your :type.',
     ],
 
     'seo_noindex' => [
@@ -123,13 +123,13 @@ return [
     ],
 
     'seo_section_sitemap' => [
-        'instructions' => 'Configure the sitemap settings for this :type.',
-        'default_instructions' => 'Configure the default sitemap settings for your :type.',
+        'instructions' => 'Configure the sitemap settings of this :type.',
+        'default_instructions' => 'Configure the default sitemap settings of your :type.',
     ],
 
     'seo_sitemap_priority' => [
         'instructions' => 'Choose the sitemap priority of this :type. `1.0` is the most important.',
-        'default_instructions' => 'Choose the default sitemap priority for your :type. `1.0` is the most important.',
+        'default_instructions' => 'Choose the default sitemap priority of your :type. `1.0` is the most important.',
     ],
 
     'seo_sitemap_change_frequency' => [
@@ -143,8 +143,8 @@ return [
     ],
 
     'seo_json_ld' => [
-        'instructions' => 'Structured data for this :type. This will be wrapped in the appropriate script tag.',
-        'default_instructions' => 'Default structured data for your :type. This will be wrapped in the appropriate script tag.',
+        'instructions' => 'The structured data of this :type. This will be wrapped in the appropriate script tag.',
+        'default_instructions' => 'The default structured data of your :type. This will be wrapped in the appropriate script tag.',
     ],
 
 ];
