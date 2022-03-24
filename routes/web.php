377a1e75e7165dd7b1f9c29a44562d68c04099e4
Michael Aerni <hello@michaelaerni.ch>
2022-03-24 10:31:15 -0700

    Fix issue with route caching

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Aerni\AdvancedSeo\Http\Controllers\Web\SitemapController;
-use Illuminate\Support\Facades\Route;
+use Statamic\Facades\URL;
 use Statamic\Facades\Site;
+use Illuminate\Support\Facades\Route;
 
 Route::name('advanced-seo.')->group(function () {
-    Site::all()->map(function ($site) {
-        Route::get($site->url() . '/sitemap.xml', [SitemapController::class, 'index'])->name('sitemap.index');
-        Route::get($site->url() . '/sitemap_{type}_{handle}.xml', [SitemapController::class, 'show'])->name('sitemap.show');
-        Route::get($site->url() . '/sitemap.xsl', [SitemapController::class, 'xsl'])->name('sitemap.xsl');
-    });
+    Route::get(URL::tidy(Site::current()->url() . '/sitemap.xml'), [SitemapController::class, 'index'])->name('sitemap.index');
+    Route::get(URL::tidy(Site::current()->url() . '/sitemap_{type}_{handle}.xml'), [SitemapController::class, 'show'])->name('sitemap.show');
+    Route::get(URL::tidy(Site::current()->url() . '/sitemap.xsl'), [SitemapController::class, 'xsl'])->name('sitemap.xsl');
 });
