a32582e5b4d674a5e30a9b6c9bed2ad176c1a509
Michael Aerni <hello@michaelaerni.ch>
2022-03-24 10:18:25 -0700

    Don’t loop through sites

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
+use Statamic\Facades\URL;
 use Statamic\Facades\Site;
 use Illuminate\Support\Facades\Route;
 use Aerni\AdvancedSeo\Http\Controllers\Web\SitemapController;
 use Aerni\AdvancedSeo\Http\Controllers\Web\SocialImagesController;
 
 Route::name('advanced-seo.')->group(function () {
-    Site::all()->map(function ($site) {
-        // Social Images Generator
-        Route::get($site->url() . '/social-images/{type}/{id}', [SocialImagesController::class, 'show']);
+    Route::get(URL::tidy(Site::current()->url() . '/social-images/{type}/{id}'), [SocialImagesController::class, 'show'])->name('social_images.show');
 
-        // Sitemap
-        Route::get($site->url() . '/sitemap.xml', [SitemapController::class, 'index']);
-        Route::get($site->url() . '/sitemap_{type}_{handle}.xml', [SitemapController::class, 'show']);
-        Route::get($site->url() . '/sitemap.xsl', [SitemapController::class, 'xsl']);
-    });
+    Route::get(URL::tidy(Site::current()->url() . '/sitemap.xml'), [SitemapController::class, 'index'])->name('sitemap.index');
+    Route::get(URL::tidy(Site::current()->url() . '/sitemap_{type}_{handle}.xml'), [SitemapController::class, 'show'])->name('sitemap.show');
+    Route::get(URL::tidy(Site::current()->url() . '/sitemap.xsl'), [SitemapController::class, 'xsl'])->name('sitemap.xsl');
 });
