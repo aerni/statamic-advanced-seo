e3599121e8be8bbaf88cfeefa84de45758c69bfe
Michael Aerni <hello@michaelaerni.ch>
2022-03-24 09:53:53 -0700

    Fix issue with duplicate route names

--- routes/web.php
+++ routes/web.php
@@ -8,11 +8,11 @@ use Aerni\AdvancedSeo\Http\Controllers\Web\SocialImagesController;
 Route::name('advanced-seo.')->group(function () {
     Site::all()->map(function ($site) {
         // Social Images Generator
-        Route::get($site->url() . '/social-images/{type}/{id}', [SocialImagesController::class, 'show'])->name('social_images.show');
+        Route::get($site->url() . '/social-images/{type}/{id}', [SocialImagesController::class, 'show']);
 
         // Sitemap
-        Route::get($site->url() . '/sitemap.xml', [SitemapController::class, 'index'])->name('sitemap.index');
-        Route::get($site->url() . '/sitemap_{type}_{handle}.xml', [SitemapController::class, 'show'])->name('sitemap.show');
-        Route::get($site->url() . '/sitemap.xsl', [SitemapController::class, 'xsl'])->name('sitemap.xsl');
+        Route::get($site->url() . '/sitemap.xml', [SitemapController::class, 'index']);
+        Route::get($site->url() . '/sitemap_{type}_{handle}.xml', [SitemapController::class, 'show']);
+        Route::get($site->url() . '/sitemap.xsl', [SitemapController::class, 'xsl']);
     });
 });
