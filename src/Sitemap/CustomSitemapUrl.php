a209a0075f3e16db1ad99b2d49c6d9bf06d8a8b5
Michael Aerni <hello@michaelaerni.ch>
2022-04-04 16:05:11 -0700

    Add guards for custom sitemap url values

--- src/Sitemap/CustomSitemapUrl.php
+++ src/Sitemap/CustomSitemapUrl.php
@@ -3,6 +3,7 @@
 namespace Aerni\AdvancedSeo\Sitemap;
 
 use Aerni\AdvancedSeo\Models\Defaults;
+use Illuminate\Support\Carbon;
 use Statamic\Support\Traits\FluentlyGetsAndSets;
 
 class CustomSitemapUrl extends BaseSitemapUrl
@@ -32,11 +33,14 @@ class CustomSitemapUrl extends BaseSitemapUrl
             ->args(func_get_args());
     }
 
-    public function lastmod(string $lastmod = null): string|self|null
+    public function lastmod(Carbon $lastmod = null): string|self|null
     {
         return $this->fluentlyGetOrSet('lastmod')
             ->getter(function () {
                 return $this->lastmod ?? now()->format('Y-m-d\TH:i:sP');
+            })
+            ->setter(function ($lastmod) {
+                return $lastmod->format('Y-m-d\TH:i:sP');
             })
             ->args(func_get_args());
     }
@@ -47,6 +51,14 @@ class CustomSitemapUrl extends BaseSitemapUrl
             ->getter(function () {
                 return $this->changefreq ?? Defaults::data('collections')->get('seo_sitemap_change_frequency');
             })
+            ->setter(function ($changefreq) {
+                $allowedValues = ['always', 'hourly', 'daily', 'weekly', 'monthly', 'yearly', 'never'];
+                $allowedValuesString = implode(', ', $allowedValues);
+
+                throw_unless(in_array($changefreq, $allowedValues), new \Exception("Make sure to use a valid 'changefreq' value. Valid values are: [$allowedValuesString]."));
+
+                return $changefreq;
+            })
             ->args(func_get_args());
     }
 
@@ -55,6 +67,14 @@ class CustomSitemapUrl extends BaseSitemapUrl
         return $this->fluentlyGetOrSet('priority')
             ->getter(function () {
                 return $this->priority ?? Defaults::data('collections')->get('seo_sitemap_priority');
+            })
+            ->setter(function ($priority) {
+                $allowedValues = ['0.0', '0.1', '0.2', '0.3', '0.4', '0.5', '0.6', '0.7', '0.8', '0.9', '1.0'];
+                $allowedValuesString = implode(', ', $allowedValues);
+
+                throw_unless(in_array($priority, $allowedValues), new \Exception("Make sure to use a valid 'priority' value. Valid values are: [$allowedValuesString]."));
+
+                return $priority;
             })
             ->args(func_get_args());
     }
