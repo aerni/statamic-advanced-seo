216fae77143f8dbf9521bdaed3fa24e5fb58b667
Michael Aerni <hello@michaelaerni.ch>
2022-04-07 12:11:56 -0700

    Reorder methods

--- src/Content/SocialImage.php
+++ src/Content/SocialImage.php
@@ -43,14 +43,14 @@ class SocialImage
         return URL::assemble(AssetContainer::find($container)->absoluteUrl(), $this->path());
     }
 
-    protected function templateUrl(): string
-    {
-        return url('/') . "/!/advanced-seo/social-images/{$this->specs['type']}/{$this->entry->id}?site={$this->entry->locale}&theme={$this->entry->seo_social_images_theme}";
-    }
-
     public function path(): string
     {
         return "social_images/{$this->entry->slug}-{$this->entry->locale}-{$this->specs['type']}.png";
+    }
+
+    protected function templateUrl(): string
+    {
+        return url('/') . "/!/advanced-seo/social-images/{$this->specs['type']}/{$this->entry->id}?site={$this->entry->locale}&theme={$this->entry->seo_social_images_theme}";
     }
 
     protected function absolutePath($path = null): string
