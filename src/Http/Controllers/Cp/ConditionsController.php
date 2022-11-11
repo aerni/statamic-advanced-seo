928aea24b5ff7dc43b350a1268e0e78f4cf98153
Michael Aerni <hello@michaelaerni.ch>
2022-11-11 16:40:34 -0500

    Make conditions controller more readable

--- src/Http/Controllers/Cp/ConditionsController.php
+++ src/Http/Controllers/Cp/ConditionsController.php
@@ -20,33 +20,15 @@ class ConditionsController extends Controller
          * If no model was found, we are parsing the URL instead.
          * This is the case when creating an entry or term.
          */
-        if ($id = $request->get('id')) {
-            $model = Data::find($id) ?? Seo::findById($id);
-            $data = GetDefaultsData::handle($model);
-        } else {
-            $url = parse_url($request->get('url')['href']);
-            $path = array_slice(explode('/', $url['path']), 2);
-
-            /**
-             * TODO: This could also be moved into the GetDefaultsData action
-             * by adding a case for a URL string. For now we leave it as is as we only use this case once.
-             */
-            $data = new DefaultsData(
-                type: $path[0],
-                handle: $path[1],
-                locale: $path[4],
-                // sites: TODO: Right now we don't need the sites.
-            );
-        }
-
-        if (! $data) {
-            return [];
-        }
+        $data = $request->get('id')
+            ? $this->getDataById($request)
+            : $this->getDataByUrl($request);
 
         /**
          * We have to manually set the locale if a site query exists.
-         * The locale can't be correctly evaluated in EvaluateModelLocale because the request
-         * is coming from this controller and doesn't have the "site" query from the original request.
+         * That's because the locale can't be correctly evaluated in the EvaluateModelLocale action.
+         * Because the request is coming from this controller and doesn't contain the "site" query from the original request.
+         * This is the case when editing an existing term.
          */
         if ($site = $request->get('site')) {
             $data->locale = $site;
@@ -56,5 +38,31 @@ class ConditionsController extends Controller
             'showSitemapFields' => ShowSitemapFields::handle($data),
             'showSocialImagesGeneratorFields' => ShowSocialImagesGeneratorFields::handle($data),
         ];
+    }
+
+    protected function getDataById(Request $request): DefaultsData
+    {
+        $id = $request->get('id');
+
+        $model = Data::find($id) ?? Seo::findById($id);
+
+        return GetDefaultsData::handle($model);
+    }
+
+    protected function getDataByUrl(Request $request): DefaultsData
+    {
+        $url = parse_url($request->get('url')['href']);
+        $path = array_slice(explode('/', $url['path']), 2);
+
+        /**
+         * TODO: This could also be moved into the GetDefaultsData action by adding a case for a URL string.
+         * For now we leave it as is as we only use this case once.
+         */
+        return new DefaultsData(
+            type: $path[0],
+            handle: $path[1],
+            locale: $path[4],
+            // sites: TODO: Right now we don't need the sites.
+        );
     }
 }
