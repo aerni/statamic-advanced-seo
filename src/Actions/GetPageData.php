bdb2bc9c3ad17e53025707e71bbadbb298ed4339
Michael Aerni <hello@michaelaerni.ch>
2023-02-04 18:49:10 -0500

    Fix error if no defaults data can be evaluated

--- src/Actions/GetPageData.php
+++ src/Actions/GetPageData.php
@@ -10,22 +10,24 @@ use Statamic\Tags\Context;
 
 class GetPageData
 {
-    public static function handle(mixed $model): ?Collection
+    public static function handle(mixed $model): Collection
     {
+        if (! $data = GetDefaultsData::handle($model)) {
+            return collect();
+        }
+
         /**
          * We only want to return data of enabled features.
          * This ensures that we don't return any values of conditionally hidden fields.
          * This would typically happen when a feature like the social images generator has been disabled.
          */
-        $fields = OnPageSeoBlueprint::make()
-            ->data(GetDefaultsData::handle($model))
-            ->items();
+        $fields = OnPageSeoBlueprint::make()->data($data)->items();
 
         return match (true) {
             ($model instanceof Context) => $model->intersectByKeys($fields),
             ($model instanceof Entry) => $model->toAugmentedCollection(array_keys($fields)),
             ($model instanceof Term) => $model->toAugmentedCollection(array_keys($fields)),
-            default => null,
+            default => collect(),
         };
     }
 }
