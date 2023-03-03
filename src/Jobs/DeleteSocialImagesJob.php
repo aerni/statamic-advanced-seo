2ae95be57f7651829d079645559d184719d0cc42
Michael Aerni <hello@michaelaerni.ch>
2023-03-03 12:47:11 -0500

    Use a job do delete social images

---
+++ src/Jobs/DeleteSocialImagesJob.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Aerni\AdvancedSeo\Jobs;
+
+use Aerni\AdvancedSeo\Facades\SocialImage;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Statamic\Contracts\Entries\Entry;
+
+class DeleteSocialImagesJob implements ShouldQueue
+{
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+
+    public function __construct(protected Entry $entry)
+    {
+        $this->queue = config('advanced-seo.social_images.generator.queue', 'default');
+    }
+
+    public function handle(): void
+    {
+        SocialImage::all($this->entry)->each->delete();
+    }
+}
