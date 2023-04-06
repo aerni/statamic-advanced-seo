7bb4afdeaef1f360eafe98dddcd73fb57ce966ae
Michael Aerni <hello@michaelaerni.ch>
2023-04-06 16:31:29 +0200

    Fix default views styling

--- resources/views/cp/taxonomies.blade.php
+++ resources/views/cp/taxonomies.blade.php
@@ -3,26 +3,26 @@
 
 @section('content')
 
-    <div class="mb-3">
+    <div class="mb-6">
         <h1>{{ __('advanced-seo::messages.taxonomies') }}</h1>
     </div>
 
-    <div class="flex flex-wrap -mx-2 widgets">
-        <div class="w-full px-2 mb-4 widget">
+    <div class="widgets @container flex flex-wrap -mx-4 py-2">
+        <div class="w-full px-4 mb-8 widget md:w-full">
             <div class="p-0 card content">
-                <div class="flex items-start p-4 border-b">
-                    <div class="w-8 h-8 mr-2 text-blue">
+                <div class="flex items-start px-8 py-8 border-b">
+                    <div class="w-8 h-8 mr-4 text-gray-800">
                         @cp_svg('icons/default/tags')
                     </div>
-                    <div class="flex-1 mt-sm">
-                        <h2>{{ __('advanced-seo::messages.taxonomies') }}</h2>
+                    <div class="flex-1">
+                        <h3 class="mb-2 text-blue">{{ __('advanced-seo::messages.taxonomies') }}</h3>
                         <p>{{ __('advanced-seo::messages.taxonomies_description') }}</p>
                     </div>
                 </div>
-                <div class="p-2">
+                <div class="px-6 py-6">
                     @foreach (Aerni\AdvancedSeo\Models\Defaults::enabledInType('taxonomies') as $taxonomy)
                         @can("view seo {$taxonomy['handle']} defaults")
-                            <a href="{{ cp_route('advanced-seo.taxonomies.edit', $taxonomy['handle']) }}" class="block px-1.5 py-1 text-sm rounded-md hover:bg-blue-100">{{ $taxonomy['title'] }}</a>
+                            <a href="{{ cp_route('advanced-seo.taxonomies.edit', $taxonomy['handle']) }}" class="block px-3 py-2 -mx-1 text-sm rounded-md hover:bg-gray-200">{{ $taxonomy['title'] }}</a>
                         @endcan
                     @endforeach
                 </div>
