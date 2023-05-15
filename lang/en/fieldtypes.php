8228c1d63d7211f48001532abbca2eb2bad05b29
Michael Aerni <hello@michaelaerni.ch>
2023-05-15 15:09:21 -0400

    Better field instructions

--- lang/en/fieldtypes.php
+++ lang/en/fieldtypes.php
@@ -6,9 +6,9 @@ return [
         'config' => [
             'field' => [
                 'display' => 'Field',
-                'instructions' => 'The Advanced SEO field you want to use.',
+                'instructions' => 'The Advanced SEO field you want to use. Make sure to set the `handle` of this field to the one shown in the dropdown.',
             ],
-        ]
+        ],
     ],
 
 ];
