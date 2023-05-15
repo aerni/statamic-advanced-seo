1eb9e47851cbf7f669841925cf09528ea6c8c8ba
Michael Aerni <hello@michaelaerni.ch>
2023-05-15 15:10:32 -0400

    Fix blueprint editor exception (#70)

--- src/Fieldtypes/AdvancedSeoFieldtype.php
+++ src/Fieldtypes/AdvancedSeoFieldtype.php
@@ -21,13 +21,18 @@ class AdvancedSeoFieldtype extends Fieldtype
     protected function configFieldItems(): array
     {
         return [
-            'field' => [
-                'display' => __('advanced-seo::fieldtypes.advanced_seo.config.field.display'),
-                'instructions' => __('advanced-seo::fieldtypes.advanced_seo.config.field.instructions'),
-                'type' => 'select',
-                'width' => 50,
-                'options' => $this->options(),
-                'validate' => 'required',
+            [
+                'display' => __('Configuration'),
+                'fields' => [
+                    'field' => [
+                        'display' => __('advanced-seo::fieldtypes.advanced_seo.config.field.display'),
+                        'instructions' => __('advanced-seo::fieldtypes.advanced_seo.config.field.instructions'),
+                        'type' => 'select',
+                        'width' => 50,
+                        'options' => $this->options(),
+                        'validate' => 'required',
+                    ],
+                ],
             ],
         ];
     }
