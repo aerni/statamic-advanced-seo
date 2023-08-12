dc6410926a5d2c7a15e1d6cba202d593d09b5c3a
Michael Aerni <hello@michaelaerni.ch>
2023-08-12 16:23:38 -0400

    Fix issue when `short_open_tag` is on

--- resources/views/sitemaps/index.blade.php
+++ resources/views/sitemaps/index.blade.php
@@ -1,5 +1,7 @@
-<?xml version="1.0" encoding="utf-8"?>
-<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
+@php
+echo '<?xml version="1.0" encoding="utf-8"?>';
+echo '<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>';
+@endphp
 
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" advanced-seo-version="{{ $version }}">
     @foreach ($sitemaps as $sitemap)
