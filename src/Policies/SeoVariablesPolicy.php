04f71a8ba86a078d6276a4a5f1bf5d5be92f047c
Michael Aerni <hello@michaelaerni.ch>
2023-11-24 21:23:53 +0100

    Fix policy with users in database (#110)

--- src/Policies/SeoVariablesPolicy.php
+++ src/Policies/SeoVariablesPolicy.php
@@ -3,28 +3,27 @@
 namespace Aerni\AdvancedSeo\Policies;
 
 use Aerni\AdvancedSeo\Models\Defaults;
-use Statamic\Contracts\Auth\User;
-use Statamic\Facades\User as UserFacade;
+use Statamic\Facades\User;
 
 class SeoVariablesPolicy
 {
-    public function index(User $user, string $type): bool
+    public function index($user, string $type): bool
     {
         return Defaults::enabledInType($type)->map->handle
             ->filter(fn ($type) => $this->view($user, $type))
             ->isNotEmpty();
     }
 
-    public function view(User $user, string $type): bool
+    public function view($user, string $type): bool
     {
-        $user = UserFacade::fromUser($user);
+        $user = User::fromUser($user);
 
         return $user->hasPermission("view seo {$type} defaults");
     }
 
-    public function edit(User $user, string $type): bool
+    public function edit($user, string $type): bool
     {
-        $user = UserFacade::fromUser($user);
+        $user = User::fromUser($user);
 
         return $user->hasPermission("edit seo {$type} defaults");
     }
