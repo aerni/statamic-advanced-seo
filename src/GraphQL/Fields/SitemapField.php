8ed5f7de389373ae760fde85a10ac628b35b2724
Michael Aerni <hello@michaelaerni.ch>
2024-07-17 13:09:46 -0400

    Get rid of SitemapIndex for SitemapRepository

--- src/GraphQL/Fields/SitemapField.php
+++ src/GraphQL/Fields/SitemapField.php
@@ -2,13 +2,13 @@
 
 namespace Aerni\AdvancedSeo\GraphQL\Fields;
 
-use Aerni\AdvancedSeo\GraphQL\Types\SeoSitemapType;
-use Aerni\AdvancedSeo\Sitemaps\SitemapIndex;
-use GraphQL\Type\Definition\ResolveInfo;
-use GraphQL\Type\Definition\Type;
-use Illuminate\Support\Collection;
-use Rebing\GraphQL\Support\Field;
 use Statamic\Facades\GraphQL;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Field;
+use Illuminate\Support\Collection;
+use Aerni\AdvancedSeo\Facades\Sitemap;
+use GraphQL\Type\Definition\ResolveInfo;
+use Aerni\AdvancedSeo\GraphQL\Types\SeoSitemapType;
 
 class SitemapField extends Field
 {
@@ -42,7 +42,7 @@ class SitemapField extends Field
 
     public function resolve($root, $args, $context, ResolveInfo $info): ?Collection
     {
-        $sitemaps = (new SitemapIndex)->{"{$info->fieldName}Sitemaps"}();
+        $sitemaps = Sitemap::{"{$info->fieldName}Sitemaps"}();
 
         if ($baseUrl = $args['baseUrl'] ?? null) {
             $sitemaps = $sitemaps->each(fn ($sitemap) => $sitemap->baseUrl($baseUrl));
--- src/Sitemaps/SitemapRepository.php
+++ src/Sitemaps/SitemapRepository.php
@@ -2,36 +2,69 @@
 
 namespace Aerni\AdvancedSeo\Sitemaps;
 
+use Illuminate\Support\Collection;
 use Aerni\AdvancedSeo\Contracts\Sitemap;
+use Aerni\AdvancedSeo\Actions\IsEnabledModel;
 use Aerni\AdvancedSeo\Sitemaps\Custom\CustomSitemap;
 use Aerni\AdvancedSeo\Sitemaps\Custom\CustomSitemapUrl;
-use Illuminate\Support\Collection;
+use Aerni\AdvancedSeo\Sitemaps\Taxonomies\TaxonomySitemap;
+use Aerni\AdvancedSeo\Sitemaps\Collections\CollectionSitemap;
+use Statamic\Facades\Collection as CollectionFacade;
+use Statamic\Facades\Taxonomy;
 
 class SitemapRepository
 {
+    protected array $customSitemaps = [];
+
     public function make(string $handle): CustomSitemap
     {
         return new CustomSitemap($handle);
     }
 
-    public static function makeUrl(string $loc): CustomSitemapUrl
+    public function makeUrl(string $loc): CustomSitemapUrl
     {
         return new CustomSitemapUrl($loc);
     }
 
     public function add(CustomSitemap $sitemap): void
     {
-        SitemapIndex::add($sitemap);
+        $this->customSitemaps = $this->customSitemaps()
+            ->push($sitemap)
+            ->unique(fn ($sitemap) => $sitemap->handle())
+            ->toArray();
     }
 
     public function all(): Collection
     {
-        return (new SitemapIndex)->sitemaps();
+        return $this->collectionSitemaps()
+            ->merge($this->taxonomySitemaps())
+            ->merge($this->customSitemaps());
     }
 
     public function find(string $id): ?Sitemap
     {
         return $this->all()->first(fn ($sitemap) => $id === $sitemap->id());
+    }
+
+    public function collectionSitemaps(): Collection
+    {
+        return CollectionFacade::all()
+            ->filter(IsEnabledModel::handle(...))
+            ->map(fn ($collection) => new CollectionSitemap($collection))
+            ->values();
+    }
+
+    public function taxonomySitemaps(): Collection
+    {
+        return Taxonomy::all()
+            ->filter(IsEnabledModel::handle(...))
+            ->map(fn ($taxonomy) => new TaxonomySitemap($taxonomy))
+            ->values();
+    }
+
+    public function customSitemaps(): Collection
+    {
+        return collect($this->customSitemaps);
     }
 
     public function clearCache(): void
