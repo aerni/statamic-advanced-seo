eaee7c01ec5cfc9760138c263b43b04179495830
Michael Aerni <hello@michaelaerni.ch>
2025-02-10 12:42:56 -0500

    Fix tests

---
+++ tests/SeoDefaultSetEloquentTest.php
@@ -0,0 +1,9 @@
+<?php
+
+beforeEach(function () {
+    copy(__DIR__.'/__fixtures__/composer.eloquent.lock', __DIR__.'/../vendor/orchestra/testbench-core/laravel/composer.lock');
+});
+
+it('test', function () {
+
+});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -19,4 +19,22 @@ abstract class TestCase extends AddonTestCase
 
         $app['config']->set('advanced-seo.directory', __DIR__.'/__fixtures__/content/seo');
     }
+
+    protected function setUp(): void
+    {
+        /* Need to copy this file to the correct location so that Composer::isInstalled() won't fail in the service provider. */
+        copy(__DIR__.'/__fixtures__/composer.empty.lock', __DIR__.'/../vendor/orchestra/testbench-core/laravel/composer.lock');
+
+        parent::setUp();
+    }
+
+    protected function teardown(): void
+    {
+        /* Delete the previously copied composer.lock file. */
+        unlink(__DIR__.'/../vendor/orchestra/testbench-core/laravel/composer.lock');
+
+        parent::teardown();
+    }
+
+
 }
