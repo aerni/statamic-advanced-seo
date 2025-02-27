6845c0dee990f24844a7c2b91e3fc077889f2367
Brian Faust <hello@basecode.sh>
2025-02-27 10:37:42 +0200

    Only register sitemap routes if sitemap is enabled

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Aerni\AdvancedSeo\Http\Controllers\Web\SitemapController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('advanced-seo.')->group(function () {
-    Route::get('/sitemap.xml', [SitemapController::class, 'index'])->name('sitemap.index');
-    Route::get('/sitemaps/{id}.xml', [SitemapController::class, 'show'])->name('sitemap.show');
-    Route::get('/sitemap.xsl', [SitemapController::class, 'xsl'])->name('sitemap.xsl');
-});
+if (config('advanced-seo.sitemap.enabled')) {
+    Route::name('advanced-seo.')->group(function () {
+        Route::get('/sitemap.xml', [SitemapController::class, 'index'])->name('sitemap.index');
+        Route::get('/sitemaps/{id}.xml', [SitemapController::class, 'show'])->name('sitemap.show');
+        Route::get('/sitemap.xsl', [SitemapController::class, 'xsl'])->name('sitemap.xsl');
+    });
+}
