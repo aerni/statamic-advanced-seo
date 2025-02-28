c2078b8af5c61e3b2aab0d1a49773f9d6fd02d20
Michael Aerni <hello@michaelaerni.ch>
2025-02-28 17:11:59 -0500

    Also consider crawling config

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Aerni\AdvancedSeo\Http\Controllers\Web\SitemapController;
 use Illuminate\Support\Facades\Route;
 
-if (config('advanced-seo.sitemap.enabled')) {
+if (config('advanced-seo.sitemap.enabled') && in_array(app()->environment(), config('advanced-seo.crawling.environments', []))) {
     Route::name('advanced-seo.')->group(function () {
         Route::get('/sitemap.xml', [SitemapController::class, 'index'])->name('sitemap.index');
         Route::get('/sitemaps/{id}.xml', [SitemapController::class, 'show'])->name('sitemap.show');
