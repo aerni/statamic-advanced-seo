e5b69287ec0b24c7080b4e8918377779d5626b43
Michael Aerni <hello@michaelaerni.ch>
2025-03-26 12:05:20 -0400

    Fix migration

--- database/migrations/2025_02_05_100000_create_advanced_seo_defaults_table.php
+++ database/migrations/2025_02_05_100000_create_advanced_seo_defaults_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create($this->prefix('advanced_seo_defaults'), function (Blueprint $table) {
             $table->id();
             $table->string('type');
-            $table->string('handle')->unique();
+            $table->string('handle');
             $table->jsonb('data');
             $table->timestamps();
         });
