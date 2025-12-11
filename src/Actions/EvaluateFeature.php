84394ef7960dd3dc51aed20b134cd24a0d14caf5
Michael Aerni <hello@michaelaerni.ch>
2025-12-11 14:29:40 +0100

    No need for resolve

--- src/Actions/EvaluateFeature.php
+++ src/Actions/EvaluateFeature.php
@@ -10,7 +10,7 @@ class EvaluateFeature
     public static function handle(string $feature, ?DefaultsData $data = null): bool
     {
         return $data
-            ? Blink::once("advanced-seo::features::{$feature}::{$data->id()}", fn () => resolve($feature)::enabled($data))
-            : Blink::once("advanced-seo::features::{$feature}", fn () => resolve($feature)::enabled());
+            ? Blink::once("advanced-seo::features::{$feature}::{$data->id()}", fn () => $feature::enabled($data))
+            : Blink::once("advanced-seo::features::{$feature}", fn () => $feature::enabled());
     }
 }
