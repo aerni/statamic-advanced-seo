bd28c0c3fbd17fe501db203696d4f9933b714f98
Michael Aerni <hello@michaelaerni.ch>
2026-01-05 18:49:47 +0100

    Ensure data integrity when sites change

--- src/Subscribers/SeoSetSubscriber.php
+++ src/Subscribers/SeoSetSubscriber.php
@@ -12,18 +12,34 @@ class SeoSetSubscriber
     public function subscribe(Dispatcher $events): array
     {
         return [
+            Events\CollectionSaved::class => 'saveSeoSet',
             Events\CollectionDeleted::class => 'deleteSeoSet',
+            Events\TaxonomySaved::class => 'saveSeoSet',
             Events\TaxonomyDeleted::class => 'deleteSeoSet',
         ];
     }
 
+    public function saveSeoSet(Event $event): void
+    {
+        $seoSet = Seo::find($this->getId($event));
+
+        if (! $seoSet || ! $seoSet->enabled()) {
+            return;
+        }
+
+        $seoSet->save();
+    }
+
     public function deleteSeoSet(Event $event): void
     {
-        $id = match (true) {
+        Seo::find($this->getId($event))?->delete();
+    }
+
+    protected function getId(Event $event): string
+    {
+        return match (true) {
             property_exists($event, 'collection') => "collections::{$event->collection->handle()}",
             property_exists($event, 'taxonomy') => "taxonomies::{$event->taxonomy->handle()}",
         };
-
-        Seo::find($id)?->delete();
     }
 }
