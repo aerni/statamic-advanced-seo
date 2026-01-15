ac3cb72f1b349e72a1251c92dc75603469bbaea0
Michael Aerni <hello@michaelaerni.ch>
2026-01-15 11:17:01 +0100

    Allow registries to return a custom collection

--- src/Registries/Registry.php
+++ src/Registries/Registry.php
@@ -7,10 +7,12 @@ use Statamic\Facades\Blink;
 
 abstract class Registry
 {
+    protected string $collection = Collection::class;
+
     abstract protected function items(): array;
 
     public function all(): Collection
     {
-        return Blink::once(static::class.'::all', fn () => collect($this->items()));
+        return Blink::once(static::class.'::all', fn () => new $this->collection($this->items()));
     }
 }
