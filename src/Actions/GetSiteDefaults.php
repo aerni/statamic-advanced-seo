a75ea00797b7ecf6aba245066a4e835f95e9e5d6
Michael Aerni <hello@michaelaerni.ch>
2026-03-28 10:32:28 +0100

    Remove ability to override site defaults for custom routes

--- src/Actions/GetSiteDefaults.php
+++ src/Actions/GetSiteDefaults.php
@@ -7,8 +7,6 @@ use Aerni\AdvancedSeo\Facades\Seo;
 use Illuminate\Support\Collection;
 use Statamic\Facades\Blink;
 use Statamic\Facades\Site;
-use Statamic\Fields\Value;
-use Statamic\Tags\Context as ViewContext;
 
 class GetSiteDefaults
 {
@@ -16,37 +14,8 @@ class GetSiteDefaults
     {
         $site = Context::from($model)?->site ?? Site::current()->handle();
 
-        return Blink::once("advanced-seo::site::{$site}", function () use ($site, $model) {
-            $siteDefaults = Seo::find('site::defaults')->in($site)->toAugmentedCollection();
-
-            /**
-             * TODO: Instead of merging the overrides, we might be able to refactor this to something similar to the GetPageData action.
-             * Instead of augmenting all the default values upfront, we could get the blueprint of each site default and then add the values to each blueprint field.
-             */
-            if ($model instanceof ViewContext) {
-                return self::mergeViewOverrides($siteDefaults, $model);
-            }
-
-            return $siteDefaults;
+        return Blink::once("advanced-seo::site::{$site}", function () use ($site) {
+            return Seo::find('site::defaults')->in($site)->toAugmentedCollection();
         });
-    }
-
-    /**
-     * Allow overriding site defaults by matching key in the data.
-     * This is useful if you want to override the site default when working with custom views.
-     * We need to create a new value object because we can't simply change the `value` in the existing object.
-     */
-    protected static function mergeViewOverrides(Collection $siteDefaults, Collection $overrides): Collection
-    {
-        $overrides = $siteDefaults
-            ->intersectByKeys($overrides)
-            ->map(fn ($originalValue, $key) => new Value(
-                value: ($value = $overrides->get($key)) instanceof Value ? $value->raw() : $value,
-                handle: $originalValue->handle(),
-                fieldtype: $originalValue->fieldtype(),
-                augmentable: $originalValue->augmentable()
-            ));
-
-        return $siteDefaults->merge($overrides);
     }
 }
