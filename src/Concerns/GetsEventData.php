32ef996174d5fbb2b208229ea3ea176a9d00bb3a
Michael Aerni <hello@michaelaerni.ch>
2026-04-18 10:48:12 +0200

    Remove stale SeoSetDefaults reference from event property resolution

--- src/Concerns/GetsEventData.php
+++ src/Concerns/GetsEventData.php
@@ -9,9 +9,7 @@ trait GetsEventData
 {
     protected function getProperty(Event $event): mixed
     {
-        // TODO: Will this throw an error somewhere because "defaults" likely belongs
-        // to the deprecated SeoSetDefaults class?
-        $property = collect(['collection', 'entry', 'taxonomy', 'term', 'defaults'])
+        $property = collect(['collection', 'entry', 'taxonomy', 'term'])
             ->first(fn ($property) => property_exists($event, $property));
 
         return $event->$property;
