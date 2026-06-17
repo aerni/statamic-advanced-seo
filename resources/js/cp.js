9ca33ae8bd97e7a29dc486f743823f6a58d1592a
Michael Aerni <hello@michaelaerni.ch>
2026-06-17 14:22:14 +0200

    Render SEO field values in entry and term listings (#221)

--- resources/js/cp.js
+++ resources/js/cp.js
@@ -2,6 +2,7 @@ import Dashboard from './pages/Dashboard.vue';
 import Index from './pages/Index.vue';
 import Edit from './pages/Edit.vue';
 import SeoFieldtype from './components/fieldtypes/SeoFieldtype.vue'
+import SeoIndexFieldtype from './components/fieldtypes/SeoIndexFieldtype.vue'
 import SiteOriginsFieldtype from './components/fieldtypes/SiteOriginsFieldtype.vue'
 import SearchPreviewFieldtype from './components/fieldtypes/SearchPreviewFieldtype.vue'
 import SocialPreviewFieldtype from './components/fieldtypes/SocialPreviewFieldtype.vue'
@@ -17,6 +18,7 @@ Statamic.booting(() => {
     Statamic.$inertia.register('advanced-seo::Taxonomies/Index', Index)
     Statamic.$inertia.register('advanced-seo::Taxonomies/Edit', Edit)
     Statamic.$components.register('seo-fieldtype', SeoFieldtype)
+    Statamic.$components.register('seo-fieldtype-index', SeoIndexFieldtype)
     Statamic.$components.register('site_origins-fieldtype', SiteOriginsFieldtype)
     Statamic.$components.register('search_preview-fieldtype', SearchPreviewFieldtype)
     Statamic.$components.register('social_preview-fieldtype', SocialPreviewFieldtype)
