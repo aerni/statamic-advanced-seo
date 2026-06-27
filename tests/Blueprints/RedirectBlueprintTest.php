6c8f488ee3e875c70f6f1b9e047ebca5b94bcbc3
Michael Aerni <hello@michaelaerni.ch>
2026-06-27 10:16:25 -0400

    Test that the redirect site field filters to authorized sites

--- tests/Blueprints/RedirectBlueprintTest.php
+++ tests/Blueprints/RedirectBlueprintTest.php
@@ -1,6 +1,8 @@
 <?php
 
 use Aerni\AdvancedSeo\Blueprints\RedirectBlueprint;
+use Statamic\Facades\Role;
+use Statamic\Facades\Site;
 use Statamic\Facades\User;
 
 beforeEach(function () {
@@ -24,4 +26,33 @@ it('hides the destination for a gone redirect', function () {
     $field = RedirectBlueprint::definition()->field('destination');
 
     expect($field->config())->toHaveKey('if');
+});
+
+it('shows all sites to a super user in the site field', function () {
+    Site::setSites([
+        'default' => ['name' => 'Default', 'url' => '/', 'locale' => 'en'],
+        'french' => ['name' => 'French', 'url' => '/fr/', 'locale' => 'fr'],
+    ]);
+
+    $super = tap(User::make()->makeSuper())->save();
+    $this->actingAs($super);
+
+    $options = RedirectBlueprint::definition()->field('site')->config()['options'];
+
+    expect(array_keys($options))->toContain('default', 'french');
+});
+
+it('filters the site field to only authorized sites for a non-super user', function () {
+    Site::setSites([
+        'default' => ['name' => 'Default', 'url' => '/', 'locale' => 'en'],
+        'french' => ['name' => 'French', 'url' => '/fr/', 'locale' => 'fr'],
+    ]);
+
+    $role = tap(Role::make('editor')->addPermission('access default site'))->save();
+    $user = tap(User::make()->assignRole('editor'))->save();
+    $this->actingAs($user);
+
+    $options = RedirectBlueprint::definition()->field('site')->config()['options'];
+
+    expect(array_keys($options))->toContain('default')->not->toContain('french');
 });
